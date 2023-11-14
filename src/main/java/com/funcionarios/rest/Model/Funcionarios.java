f250593ce764f78b51358fd1b8b1d5ae5ef7ed28
Eduardo-Leite0 <147563973+Eduardo-Leite0@users.noreply.github.com>
2023-11-14 18:10:25 -0300

    Update Funcionarios.java

--- src/main/java/com/funcionarios/rest/Model/Funcionarios.java
+++ src/main/java/com/funcionarios/rest/Model/Funcionarios.java
@@ -1,7 +1,6 @@
 package com.funcionarios.rest.Model;
 
 
-
 import java.util.Objects;
 
 import jakarta.persistence.Column;
@@ -12,12 +11,116 @@ import jakarta.persistence.Id;
 
 @Entity
 public class Funcionarios {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id;
-	
-	@Column(nullable=false)
-	private String nome;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private String sobrenome;
+
+    @Column(nullable = false)
+    private String cpf;
+
+    @Column(nullable = false)
+    private int idade;
+
+    @Column(nullable = false)
+    private String telefone;
+
+    @Column(nullable = false)
+    private String telefoneEmergencia;
+
+    @Column(nullable = false)
+    private String cargo;
+
+    @Column(nullable = false)
+    private int tempoContrato;
+
+    @Column(nullable = false)
+    private boolean ativo = true; // Por padrão, consideramos que um funcionário está ativo
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSobrenome() {
+		return sobrenome;
+	}
+
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getTelefoneEmergencia() {
+		return telefoneEmergencia;
+	}
+
+	public void setTelefoneEmergencia(String telefoneEmergencia) {
+		this.telefoneEmergencia = telefoneEmergencia;
+	}
+
+	public String getCargo() {
+		return cargo;
+	}
+
+	public void setCargo(String cargo) {
+		this.cargo = cargo;
+	}
+
+	public int getTempoContrato() {
+		return tempoContrato;
+	}
+
+	public void setTempoContrato(int tempoContrato) {
+		this.tempoContrato = tempoContrato;
+	}
+
+	public boolean isAtivo() {
+		return ativo;
+	}
+
+	public void setAtivo(boolean ativo) {
+		this.ativo = ativo;
+	}
 
 	@Override
 	public int hashCode() {
@@ -36,24 +139,5 @@ public class Funcionarios {
 		return id == other.id;
 	}
 
-	public long getId() {
-		return id;
-	}
-
-	public void setId(long id) {
-		this.id = id;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	
-
-	
 
 }
